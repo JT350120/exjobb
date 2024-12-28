48da34d966d96845c33d19263eac781256440c93
Johan <johantoerme@live.se>
2024-12-28 18:40:56 +0100

    Vite, Vue components, Tailwind initial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+  import Hero from './components/Hero.vue'
+  import Configurator from './components/Configurator.vue'
+  import Dealers from './components/Dealers.vue'
+  import History from './components/History.vue'
+  import Infobox from './components/Infobox.vue'
+</script>
+
+<template>
+  <Header />
+  <p>Content</p>
+  <Footer />
+  <!--<HelloWorld msg="Vite + Vue" />-->
+</template>
+
+<style scoped>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/*.vue",
+    "./src/*.js",
+    "./src/components/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
