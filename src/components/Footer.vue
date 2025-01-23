53364a18387d435eb83076574570339911055390
Johan <johantoerme@live.se>
2025-01-23 18:46:52 +0100

    Footer: Content and styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,47 @@
 <script setup>
-import { ref } from 'vue'
+import { useContentStore } from '../stores/content';
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+const content = useContentStore();
+const address = content.footer.address;
 </script>
 
 <template>
   <footer class="bg-black text-white px-x-standard py-y-standard">
-    <div class="flex flex-col">
-      <p>Adress</p>
-      <p>Adress</p>
-      <p>Adress</p>
+    <div class="flex relative w-full md:w-[220px]">
+      <ul class="md:border-l-2 border-b-2 border-white p-[16px] w-full">
+        <li class="first:font-bold first:mb-[8px]" v-for="(row, index) in address">{{ row }}</li>
+      </ul>
+      <div class="w-auto absolute bottom-0 left-[220px] hidden md:inline">
+        <svg width="72px" height="25px" viewBox="0 0 100.74901 35.190483" version="1.1" id="svg1"
+          inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)" sodipodi:docname="line_car.svg"
+          xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+          xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg"
+          xmlns:svg="http://www.w3.org/2000/svg">
+          <defs id="defs1" />
+          <g inkscape:label="Lager 1" inkscape:groupmode="layer" id="layer1"
+            transform="translate(-64.331587,-132.47344)">
+            <g id="g7" style="stroke-width:3;stroke-dasharray:none">
+              <g id="g9">
+                <path style="fill:none;fill-opacity:0.353868;stroke:#FCFAED;stroke-width:3;stroke-dasharray:none"
+                  d="m 65.490447,167.94543 c -1.98923,-8.8844 16.035316,-28.37741 23.434778,-31.33719 8.342773,-3.33711 21.303615,-3.9248 30.370895,-2.11135 7.67791,1.53559 9.67102,13.81717 16.0787,16.07871 12.99394,4.5861 24.47337,1.74606 28.33564,17.19514"
+                  id="car" sodipodi:nodetypes="csssc" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
     </div>
   </footer>
 </template>
 
 <style scoped>
-  footer {
-    width: 100%;
-    bottom: 0;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    z-index: 50;
-  }
+footer {
+  width: 100%;
+  bottom: 0;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 50;
+}
 </style>
--- src/stores/content.js
+++ src/stores/content.js
@@ -411,15 +411,23 @@ export const useContentStore = defineStore("content", {
                 heading: "En historia om ett geni",
                 quote: '"Jaha."',
                 quoteBy: "Elizabeth Ager om Edwards död 1936",
-                image_top: "../src/assets/backgrounds/shears.png",
-                image_middle: "../src/assets/backgrounds/shears.png",
-                image_bottom: "../src/assets/backgrounds/shears.png",
                 paragraphs: [
                     "1899 konstruerade den walesiske bonden och geniet Edward David Ager världens första ångdrivna fårsax. När affärerna gick bättre behövde han ett fordon för att ta sig runt och sälja sina produkter, och den första lastbilen med namnet Peiriant byggdes 1902.",
                     "Lastbilen blev snabbt populär och företaget utökade till att sälja personbilar. Peiriant Motor Company gjorde sig snabbt ett gott rykte som prisvärda bilar med finurliga lösningar och personlighet.",
                     "Edwards fru Elizabeth lämnade honom efter allt resande, fåren rymde och drunknade och Edward dog på trottoaren utanför en bar i London, men anden lever alltjämt kvar över företaget än idag.",
                 ]
             },
+
+        footer: {
+            address: {
+                title: "Peiriant Motor Company",
+                street: "10 Shearer Street",
+                town: "Prenynysig",
+                county: "Mid Glamorgan",
+                country: "United Kingdom",
+                zip: "CF37 4DQ",
+            },
+        }
     }),
     actions: {
     },
