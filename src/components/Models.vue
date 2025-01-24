cb7f29e68b3831cda4def5153d93a720c347fccf
Johan <johantoerme@live.se>
2025-01-24 21:49:48 +0100

    Models: min-h-full

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -8,7 +8,7 @@ const models = computed(() => content.models);
 </script>
 
 <template>
-  <div class="w-full min-h-[82vh] flex flex-col">
+  <div class="w-full min-h-full flex flex-col">
     <ModelPresentation v-for="(model, index) in models" :key="index" :indexOfModel="index" :data="model" />
   </div>
 </template>
