f80c4daaf98d715be1af7e828c1266013cd45ae8
Johan <johantoerme@live.se>
2025-01-26 21:02:15 +0100

    Converted pictures to .webp, added truck picture to history

--- src/components/History.vue
+++ src/components/History.vue
@@ -8,15 +8,15 @@ const history = content.history;
 <template>
   <div class="flex flex-col">
 
-    <section class="w-full h-[82vh] flex flex-col md:flex-row bg-green border-b-2">
+    <section class="w-full min-h-[82vh] flex flex-col md:flex-row bg-green border-b-2">
       <div class="bg-white md:w-[50%] h-auto md:h-full flex justify-center items-center border-black md:border-r-2">
         <h1 class="text-xl md:text-2xl px-x-standard py-y-standard">{{ history.heading }}</h1>
       </div>
-      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover" :style="{ backgroundImage: `url('${history.image_top}')` }"></div>
+      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover bg-edward"></div>
     </section>
 
-    <section class="w-full h-[82vh] flex flex-col-reverse md:flex-row bg-green border-b-2">
-      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover border-black md:border-r-2" :style="{ backgroundImage: `url('${history.image_middle}')` }"></div>
+    <section class="w-full min-h-[82vh] flex flex-col-reverse md:flex-row bg-green border-b-2">
+      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover bg-truck bg-center border-black md:border-r-2"></div>
       <div class="bg-green md:w-[50%] h-full flex flex-col justify-evenly py-y-standard px-x-standard">
         <p v-show="history.paragraphs[0]">{{ history.paragraphs[0] }}</p>
         <p v-show="history.paragraphs[1]">{{ history.paragraphs[1] }}</p>
@@ -33,7 +33,7 @@ const history = content.history;
           <span class="text-md text-right italic">{{ "- " + history.quoteBy }}</span>
         </div>
       </div>
-      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover bg-edward"></div>
+      <div class="md:w-[50%] h-full px-x-standard py-y-standard bg-cover"></div>
     </section>
 
   </div>
