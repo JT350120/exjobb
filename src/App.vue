e738e93f5363f190ffe6ec46032e1e8060856874
Johan <johantoerme@live.se>
2025-01-30 21:51:20 +0100

    Added favicon and removed unused component imports in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
   import Header from './components/Header.vue'
   import Footer from './components/Footer.vue'
   import Hero from './components/Hero.vue'
   import Configurator from './components/Configurator.vue'
   import Dealers from './components/Dealers.vue'
   import History from './components/History.vue'
-  import Infobox from './components/Infobox.vue'
   import Menu from './components/Menu.vue'
   import Models from './components/Models.vue'
   import Explore from './components/Explore.vue'
